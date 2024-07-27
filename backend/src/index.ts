5a9d8d22bba75065203f859e7813e2d0d9428e74
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-07-27 23:15:59 +0530

    index.ts updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,82 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono()
 
-app.post('/api/vi/signup', (c) => {
+app.use('/api/v1/blog/*', async (c,next) => {
+  const header = c.req.header("authorization") || "";
+
+  //@ts-ignore
+  const response = await verify(header, c.env.JWT_SECRET)
+  if(response.id){
+    next()
+  } else {
+    c.status(403)
+    return c.json({error: "unauthorized"})
+  }
+})
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  })
+  //@ts-ignore
+   const token = await sign({id: user.id}, c.env.JWT_SECRET)
+
+   return c.json({
+    jwt: token
+   })
+})
+
+
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+    //@ts-ignore
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+    //@ts-ignore
+		where: {
+			email: body.email,
+      password: body.password
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+  //@ts-ignore
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
+})
+
+
+app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/vi/signin', (c) => {
+app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/vi/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.put('/api/vi/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/vi/blog/:id', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
